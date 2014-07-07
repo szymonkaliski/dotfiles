c9a725d99dff3250eda8a510c6260b4ae541957c
Szymon Kaliski <kaliskiszymon@gmail.com>
2014-07-07 19:34:38 +0200

    hydra config rewritten with extensions, and HOF for hotkeys; also forcing hjkl in tmux

--- Dotfiles/hydra.lua
+++ Dotfiles/hydra.lua
@@ -2,18 +2,19 @@
 autolaunch.set(true)
 
 -- watch for changes
-pathwatcher.new(os.getenv("HOME") .. "/.hydra/", hydra.reload):start()
+-- pathwatcher.new(os.getenv("HOME") .. "/.hydra/", hydra.reload):start()
 
 -- notify on start
 notify.show("Hydra", "Started!", "", "")
 
+-- extension for frame
+ext.frame = {}
+
 -- window margin
 local margin = 10
 
--- push to screen edge
-function push(win, direction)
-	local screen = win:screen():frame_without_dock_or_menu()
-
+-- returns frame pushed to screen edge
+function ext.frame.push(screen, direction)
 	local frames = {
 		[ "up" ] = function()
 			return {
@@ -52,14 +53,11 @@ function push(win, direction)
 		end
 	}
 
-	win:setframe(frames[direction]())
+	return frames[direction]()
 end
 
--- move by margin
-function nudge(win, direction)
-	local screen = win:screen():frame_without_dock_or_menu()
-	local frame = win:frame()
-
+-- returns frame moved by margin
+function ext.frame.nudge(frame, screen, direction)
 	local modifyframe = {
 		[ "up" ] = function(frame)
 			frame.y = math.max(screen.y + margin, frame.y - margin)
@@ -82,43 +80,75 @@ function nudge(win, direction)
 		end
 	}
 
-	win:setframe(modifyframe[direction](frame))
+	return modifyframe[direction](frame)
 end
 
--- fit inside screen
-function fitframe(screen, frame)
+-- window extension
+ext.win = {}
+
+-- ugly fix for problem with window height when it's as big as screen
+function ext.win.fix(win)
+	local frame = win:frame()
+	local screen = win:screen():frame_without_dock_or_menu()
+
+	if (frame.h > (screen.h - margin * (2 - 1 / 4))) then
+		frame.h = screen.h - margin * 10
+		win:setframe(frame)
+	end
+end
+
+-- pushes window in direction and nudges to edge, fixes terminal positioning
+function ext.win.push(win, direction)
+	local screen = win:screen():frame_without_dock_or_menu()
+	local frame = win:frame()
+
+	ext.win.fix(win)
+
+	frame = ext.frame.push(screen, direction)
+	frame = ext.frame.nudge(frame, screen, direction)
+
+	win:setframe(frame)
+end
+
+-- nudges window in direction
+function ext.win.nudge(win, direction)
+	local screen = win:screen():frame_without_dock_or_menu()
+	local frame = win:frame()
+
+	frame = ext.frame.nudge(frame, screen, direction)
+
+	win:setframe(frame)
+end
+
+-- returns frame fited inside screen
+function ext.frame.fit(screen, frame)
 	frame.w = math.min(frame.w, screen.w - margin * 2)
 	frame.h = math.min(frame.h, screen.h - margin * (2 - 1 / 4))
 
 	return frame
 end
 
--- center inside screen
-function centerframe(screen, frame)
+-- returns frame centered inside screen
+function ext.frame.center(screen, frame)
 	frame.x = screen.w / 2 - frame.w / 2 + screen.x
 	frame.y = screen.h / 2 - frame.h / 2 + screen.y
 
 	return frame
 end
 
--- center given window
-function centerwindow(win)
+-- centers window
+function ext.win.center(win)
 	local screen = win:screen():frame_without_dock_or_menu()
 	local frame = win:frame()
 
-	win:setframe(centerframe(screen, frame))
+	frame = ext.frame.center(screen, frame)
+
+	win:setframe(frame)
 end
 
 -- fullscreen window with margin
-function fullscreen(win)
+function ext.win.full(win)
 	local screen = win:screen():frame_without_dock_or_menu()
-
-	-- fix for problem where window is as big as screen
-	local frame = win:frame()
-	if (frame.h > (screen.h - margin * (2 - 1 / 4))) then
-		frame.h = screen.h - margin * 10
-		win:setframe(frame)
-	end
 
 	frame = {
 		x = margin + screen.x,
@@ -127,36 +157,45 @@ function fullscreen(win)
 		h = screen.h - margin * (2 - 1 / 4)
 	}
 
+	ext.win.fix(win)
 	win:setframe(frame)
 end
 
 -- throw to next screen, center and fit
-function nextscreen(win)
+function ext.win.throw(win)
 	local screen = win:screen():next():frame_without_dock_or_menu()
 	local frame = win:frame()
 
 	frame.x = screen.x
 	frame.y = screen.y
 
-	win:setframe(centerframe(screen, fitframe(screen, frame)))
+	ext.win.fix(win)
+
+	frame = ext.frame.fit(screen, frame)
+	frame = ext.frame.center(screen, frame)
+
+	win:setframe(frame)
 	win:focus()
 end
 
 -- set window size and center
-function centerwithsize(win, w, h)
+function ext.win.size(win, size)
 	local screen = win:screen():frame_without_dock_or_menu()
 	local frame = win:frame()
 
-	frame.w = w
-	frame.h = h
+	frame.w = size.w
+	frame.h = size.h
 
-	win:setframe(centerframe(screen, fitframe(screen, frame)))
+	frame = ext.frame.fit(screen, frame)
+	frame = ext.frame.center(screen, frame)
+
+	win:setframe(frame)
 end
 
 -- save and restore window positions
 local positions = {}
 
-function winposition(win, option)
+function ext.win.pos(win, option)
 	local id = win:application():bundleid()
 	local frame = win:frame()
 
@@ -173,21 +212,27 @@ end
 
 -- cycle application windows
 -- simplified and stolen from: https://github.com/nifoc/dotfiles/blob/master/hydra/cycle.lua
-function cyclewindows(win)
+function ext.win.cycle(win)
 	local windows = win:application():visiblewindows()
+	windows = fnutils.filter(windows, function(win) return win:isstandard() end)
 
-	if #windows < 2 then
-		return nil
+	if #windows >= 2 then
+		table.sort(windows, function(a, b) return a:id() < b:id() end)
+		local activewindowindex = fnutils.indexof(windows, win)
+
+		if activewindowindex then
+			activewindowindex = activewindowindex + 1
+			if activewindowindex > #windows then activewindowindex = 1 end
+
+			windows[activewindowindex]:focus()
+		end
 	end
+end
 
-	table.sort(windows, function(a, b) return a:id() < b:id() end)
-	local activewindowindex = fnutils.indexof(windows, win)
-
-	if activewindowindex then
-		activewindowindex = activewindowindex + 1
-		if activewindowindex > #windows then activewindowindex = 1 end
-
-		windows[activewindowindex]:focus()
+-- apply function to a window with optional params
+function dowin(fn, param)
+	return function()
+		fn(window.focusedwindow(), param)
 	end
 end
 
@@ -196,25 +241,25 @@ local mod1 = { "cmd", "ctrl" }
 local mod2 = { "cmd", "ctrl", "alt" }
 
 -- window modifiers
-hotkey.bind(mod1, "c", function() centerwindow(window.focusedwindow()) end)
-hotkey.bind(mod1, "z", function() fullscreen(window.focusedwindow()) end)
-hotkey.bind(mod1, "tab", function() nextscreen(window.focusedwindow()) end)
-
--- push to edges and nudge
-fnutils.each({ "up", "down", "left", "right" }, function(direction)
-	hotkey.bind(mod1, direction, function() push(window:focusedwindow(), direction) end)
-	hotkey.bind(mod2, direction, function() nudge(window:focusedwindow(), direction) end)
-end)
+hotkey.bind(mod1, "c", dowin(ext.win.center))
+hotkey.bind(mod1, "z", dowin(ext.win.full))
+hotkey.bind(mod1, "tab", dowin(ext.win.throw))
 
 -- save and restore window positions
-hotkey.bind(mod1, "s", function() winposition(window.focusedwindow(), "save") end)
-hotkey.bind(mod1, "r", function() winposition(window.focusedwindow(), "load") end)
+hotkey.bind(mod1, "s", dowin(ext.win.pos, "save"))
+hotkey.bind(mod1, "r", dowin(ext.win.pos, "load"))
 
 -- cycle application windows
-hotkey.bind(mod1, "w", function() cyclewindows(window.focusedwindow()) end)
+hotkey.bind(mod1, "w", dowin(ext.win.cycle))
 
 -- reload hydra settings
 hotkey.bind(mod1, "h", function() hydra:reload() end)
+
+-- push to edges and nudge
+fnutils.each({ "up", "down", "left", "right" }, function(direction)
+	hotkey.bind(mod1, direction, dowin(ext.win.push, direction))
+	hotkey.bind(mod2, direction, dowin(ext.win.nudge, direction))
+end)
 
 -- set window sizes
 fnutils.each({
@@ -225,7 +270,7 @@ fnutils.each({
 	{ key = 5, w = 760, h = 620 },
 	{ key = 6, w = 770, h = 470 }
 }, function(object)
-	hotkey.bind(mod1, object.key, function() centerwithsize(window.focusedwindow(), object.w, object.h) end)
+	hotkey.bind(mod1, object.key, dowin(ext.win.size, { w = object.w, h = object.h }))
 end)
 
 -- launch and focus applications
