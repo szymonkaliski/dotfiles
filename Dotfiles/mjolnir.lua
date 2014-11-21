6db6746cd663c8fae105e43ac70277a4ca9e4417
Szymon Kaliski <kaliskiszymon@gmail.com>
2014-11-21 13:03:21 +0100

    more big updates and mpv

--- Dotfiles/mjolnir.lua
+++ Dotfiles/mjolnir.lua
@@ -8,10 +8,10 @@ local transform = require "mjolnir.sk.transform"
 
 -- extensions
 local ext = {
-	frame = {},
-	win = {},
-	app = {},
-	utils = {}
+  frame = {},
+  win = {},
+  app = {},
+  utils = {}
 }
 
 -- window extension settings
@@ -20,353 +20,370 @@ ext.win.positions  = {}
 ext.win.animate    = true
 ext.win.fixenabled = false
 
+-- returns frame resized to half of screen
+function ext.frame.resize(frame, screen, direction)
+  local frames = {
+    [ "horizontal" ] = function()
+      return {
+        x = frame.x,
+        y = frame.y,
+        h = screen.h / 2 - ext.win.margin * (2 - 1 / 4),
+        w = frame.w
+      }
+    end,
+
+    [ "vertical" ] = function()
+      return {
+        x = frame.x,
+        y = frame.y,
+        h = screen.h - ext.win.margin * (2 - 1 / 4),
+        w = screen.w / 2 - ext.win.margin * (2 - 1 / 4)
+      }
+    end
+  }
+
+  return frames[direction]()
+end
+
 -- returns frame pushed to screen edge
-function ext.frame.push(screen, direction)
-	local frames = {
-		[ "up" ] = function()
-			return {
-				x = ext.win.margin + screen.x,
-				y = ext.win.margin + screen.y,
-				w = screen.w - ext.win.margin * 2,
-				h = screen.h / 2 - ext.win.margin
-			}
-		end,
+function ext.frame.push(frame, screen, direction)
+  local frames = {
+    [ "up" ] = function()
+      return {
+        x = frame.x,
+        y = ext.win.margin + screen.y,
+        w = frame.w,
+        h = frame.h
+      }
+    end,
 
-		[ "down" ] = function()
-			return {
-				x = ext.win.margin + screen.x,
-				y = ext.win.margin * 3 / 4 + screen.h / 2 + screen.y,
-				w = screen.w - ext.win.margin * 2,
-				h = screen.h / 2 - ext.win.margin * (2 - 1 / 4)
-			}
-		end,
+    [ "down" ] = function()
+      return {
+        x = frame.x,
+        y = ext.win.margin / 2 + screen.h / 2 + screen.y,
+        w = frame.w,
+        h = frame.h
+      }
+    end,
 
-		[ "left" ] = function()
-			return {
-				x = ext.win.margin + screen.x,
-				y = ext.win.margin + screen.y,
-				w = screen.w / 2 - ext.win.margin * (2 - 1 / 4),
-				h = screen.h - ext.win.margin * (2 - 1 / 4)
-			}
-		end,
+    [ "left" ] = function()
+      return {
+        x = ext.win.margin + screen.x,
+        y = ext.win.margin + screen.y,
+        w = frame.w,
+        h = frame.h
+      }
+    end,
 
-		[ "right" ] = function()
-			return {
-				x = ext.win.margin / 2 + screen.w / 2 + screen.x,
-				y = ext.win.margin + screen.y,
-				w = screen.w / 2 - ext.win.margin * (2 - 1 / 4),
-				h = screen.h - ext.win.margin * (2 - 1 / 4)
-			}
-		end
-	}
+    [ "right" ] = function()
+      return {
+        x = ext.win.margin / 2 + screen.w / 2 + screen.x,
+        y = ext.win.margin + screen.y,
+        w = frame.w,
+        h = frame.h
+      }
+    end
+  }
 
-	return frames[direction]()
+  return frames[direction]()
 end
 
 -- returns frame moved by ext.win.margin
 function ext.frame.nudge(frame, screen, direction)
-	local modifyframe = {
-		[ "up" ] = function(frame)
-			frame.y = math.max(screen.y + ext.win.margin, frame.y - ext.win.margin)
-			return frame
-		end,
+  local modifyframe = {
+    [ "up" ] = function(frame)
+      frame.y = math.max(screen.y + ext.win.margin, frame.y - ext.win.margin)
+      return frame
+    end,
 
-		[ "down" ] = function(frame)
-			frame.y = math.min(screen.y + screen.h - frame.h - ext.win.margin * 3 / 4, frame.y + ext.win.margin)
-			return frame
-		end,
+    [ "down" ] = function(frame)
+      frame.y = math.min(screen.y + screen.h - frame.h - ext.win.margin * 3 / 4, frame.y + ext.win.margin)
+      return frame
+    end,
 
-		[ "left" ] = function(frame)
-			frame.x = math.max(screen.x + ext.win.margin, frame.x - ext.win.margin)
-			return frame
-		end,
+    [ "left" ] = function(frame)
+      frame.x = math.max(screen.x + ext.win.margin, frame.x - ext.win.margin)
+      return frame
+    end,
 
-		[ "right" ] = function(frame)
-			frame.x = math.min(screen.x + screen.w - frame.w - ext.win.margin, frame.x + ext.win.margin)
-			return frame
-		end
-	}
+    [ "right" ] = function(frame)
+      frame.x = math.min(screen.x + screen.w - frame.w - ext.win.margin, frame.x + ext.win.margin)
+      return frame
+    end
+  }
 
-	return modifyframe[direction](frame)
+  return modifyframe[direction](frame)
 end
 
 -- returns frame sent to screen edge
 function ext.frame.send(frame, screen, direction)
-	local modifyframe = {
-		[ "up" ] = function(frame)
-			frame.y = screen.y + ext.win.margin
-			return frame
-		end,
+  local modifyframe = {
+    [ "up" ] = function(frame)
+      frame.y = screen.y + ext.win.margin
+      return frame
+    end,
 
-		[ "down" ] = function(frame)
-			frame.y = screen.y + screen.h - frame.h - ext.win.margin * 3 / 4
-			return frame
-		end,
+    [ "down" ] = function(frame)
+      frame.y = screen.y + screen.h - frame.h - ext.win.margin * 3 / 4
+      return frame
+    end,
 
-		[ "left" ] = function(frame)
-			frame.x = screen.x + ext.win.margin
-			return frame
-		end,
+    [ "left" ] = function(frame)
+      frame.x = screen.x + ext.win.margin
+      return frame
+    end,
 
-		[ "right" ] = function(frame)
-			frame.x = screen.x + screen.w - frame.w - ext.win.margin
-			return frame
-		end
-	}
+    [ "right" ] = function(frame)
+      frame.x = screen.x + screen.w - frame.w - ext.win.margin
+      return frame
+    end
+  }
 
-	return modifyframe[direction](frame)
+  return modifyframe[direction](frame)
 end
 
 -- returns frame fited inside screen
 function ext.frame.fit(frame, screen)
-	frame.w = math.min(frame.w, screen.w - ext.win.margin * 2)
-	frame.h = math.min(frame.h, screen.h - ext.win.margin * (2 - 1 / 4))
+  frame.w = math.min(frame.w, screen.w - ext.win.margin * 2)
+  frame.h = math.min(frame.h, screen.h - ext.win.margin * (2 - 1 / 4))
 
-	return frame
+  return frame
 end
 
 -- returns frame centered inside screen
 function ext.frame.center(frame, screen)
-	frame.x = screen.w / 2 - frame.w / 2 + screen.x
-	frame.y = screen.h / 2 - frame.h / 2 + screen.y
+  frame.x = screen.w / 2 - frame.w / 2 + screen.x
+  frame.y = screen.h / 2 - frame.h / 2 + screen.y
 
-	return frame
+  return frame
 end
 
 -- set frame
 function ext.win.set(win, frame, time)
-	time = time or 0.15
+  time = time or 0.15
 
-	if ext.win.animate then
-		transform:setframe(win, frame, time)
-	else
-		win:setframe(frame)
-	end
+  if ext.win.animate then
+    transform:setframe(win, frame, time)
+  else
+    win:setframe(frame)
+  end
 end
 
 -- ugly fix for problem with window height when it's as big as screen
 function ext.win.fix(win)
-	if ext.win.fixenabled then
-		local screen = win:screen():frame()
-		local frame = win:frame()
+  if ext.win.fixenabled then
+    local screen = win:screen():frame()
+    local frame = win:frame()
 
-		if (frame.h > (screen.h - ext.win.margin * (2 - 1 / 4))) then
-			frame.h = screen.h - ext.win.margin * 10
-			ext.win.set(win, frame)
-		end
-	end
+    if (frame.h > (screen.h - ext.win.margin * (2 - 1 / 4))) then
+      frame.h = screen.h - ext.win.margin * 10
+      ext.win.set(win, frame)
+    end
+  end
 end
 
 -- pushes window in direction
 function ext.win.push(win, direction)
-	local screen = win:screen():frame()
-	local frame
+  local screen = win:screen():frame()
+  local frame = win:frame()
 
-	frame = ext.frame.push(screen, direction)
+  local directions = {
+    [ "up" ] = "horizontal",
+    [ "down" ] = "horizontal",
+    [ "left" ] = "vertical",
+    [ "right" ] = "vertical"
+  }
 
-	ext.win.fix(win)
-	ext.win.set(win, frame)
+  frame = ext.frame.resize(frame, screen, directions[direction])
+
+  ext.win.fix(win)
+  ext.win.set(win, frame)
+
+  frame = ext.frame.push(frame, screen, direction)
+
+  ext.win.set(win, frame)
 end
 
 -- nudges window in direction
 function ext.win.nudge(win, direction)
-	local screen = win:screen():frame()
-	local frame = win:frame()
+  local screen = win:screen():frame()
+  local frame = win:frame()
 
-	frame = ext.frame.nudge(frame, screen, direction)
-	ext.win.set(win, frame, 0.05)
+  frame = ext.frame.nudge(frame, screen, direction)
+  ext.win.set(win, frame, 0.05)
 end
 
 -- push and nudge window in direction
 function ext.win.pushandnudge(win, direction)
-	ext.win.push(win, direction)
-	ext.win.nudge(win, direction)
+  ext.win.push(win, direction)
+  ext.win.nudge(win, direction)
 end
 
 -- sends window in direction
 function ext.win.send(win, direction)
-	local screen = win:screen():frame()
-	local frame = win:frame()
+  local screen = win:screen():frame()
+  local frame = win:frame()
 
-	frame = ext.frame.send(frame, screen, direction)
+  frame = ext.frame.send(frame, screen, direction)
 
-	ext.win.fix(win)
-	ext.win.set(win, frame)
+  ext.win.fix(win)
+  ext.win.set(win, frame)
 end
 
 -- centers window
 function ext.win.center(win)
-	local screen = win:screen():frame()
-	local frame = win:frame()
+  local screen = win:screen():frame()
+  local frame = win:frame()
 
-	frame = ext.frame.center(frame, screen)
-	ext.win.set(win, frame)
+  frame = ext.frame.center(frame, screen)
+  ext.win.set(win, frame)
 end
 
 -- fullscreen window with ext.win.margin
 function ext.win.full(win)
-	local screen = win:screen():frame()
-	local frame = {
-		x = ext.win.margin + screen.x,
-		y = ext.win.margin + screen.y,
-		w = screen.w - ext.win.margin * 2,
-		h = screen.h - ext.win.margin * (2 - 1 / 4)
-	}
+  local screen = win:screen():frame()
+  local frame = {
+    x = ext.win.margin + screen.x,
+    y = ext.win.margin + screen.y,
+    w = screen.w - ext.win.margin * 2,
+    h = screen.h - ext.win.margin * (2 - 1 / 4)
+  }
 
-	-- ext.win.fix(win)
-	ext.win.set(win, frame)
+  -- ext.win.fix(win)
+  ext.win.set(win, frame)
 
-	-- center after setting frame, fixes terminal
-	ext.win.center(win)
+  -- center after setting frame, fixes terminal
+  ext.win.center(win)
 end
 
 -- throw to next screen, center and fit
-function ext.win.throw(win)
-	local screen = win:screen():next():frame()
-	local frame = win:frame()
+function ext.win.throw(win, direction)
+  local screen
 
-	frame.x = screen.x
-	frame.y = screen.y
+  if (direction == "next") then
+    screen = win:screen():next():frame()
+  else
+    screen = win:screen():previous():frame()
+  end
 
-	frame = ext.frame.fit(frame, screen)
-	frame = ext.frame.center(frame, screen)
+  local frame = win:frame()
 
-	ext.win.fix(win)
-	ext.win.set(win, frame)
+  frame.x = screen.x
+  frame.y = screen.y
 
-	win:focus()
+  frame = ext.frame.fit(frame, screen)
+  frame = ext.frame.center(frame, screen)
 
-	-- center after setting frame, fixes terminal and macvim
-	ext.win.center(win)
+  ext.win.fix(win)
+  ext.win.set(win, frame)
+
+  win:focus()
+
+  -- center after setting frame, fixes terminal and macvim
+  ext.win.center(win)
 end
 
 -- set window size and center
 function ext.win.size(win, size)
-	local screen = win:screen():frame()
-	local frame = win:frame()
+  local screen = win:screen():frame()
+  local frame = win:frame()
 
-	frame.w = size.w
-	frame.h = size.h
+  frame.w = size.w
+  frame.h = size.h
 
-	frame = ext.frame.fit(frame, screen)
-	frame = ext.frame.center(frame, screen)
+  frame = ext.frame.fit(frame, screen)
+  frame = ext.frame.center(frame, screen)
 
-	ext.win.set(win, frame)
+  ext.win.set(win, frame)
 end
 
 -- save and restore window positions
 function ext.win.pos(win, option)
-	local id = win:application():bundleid()
-	local frame = win:frame()
+  local id = win:application():bundleid()
+  local frame = win:frame()
 
-	-- saves window position if not saved before
-	if option == "save" and not ext.win.positions[id] then
-		ext.win.positions[id] = frame
-	end
+  -- saves window position if not saved before
+  if option == "save" and not ext.win.positions[id] then
+    ext.win.positions[id] = frame
+  end
 
-	-- force update saved window position
-	if option == "update" then
-		ext.win.positions[id] = frame
-	end
+  -- force update saved window position
+  if option == "update" then
+    ext.win.positions[id] = frame
+  end
 
-	-- restores window position
-	if option == "load" and ext.win.positions[id] then
-		ext.win.set(win, ext.win.positions[id])
-	end
+  -- restores window position
+  if option == "load" and ext.win.positions[id] then
+    ext.win.set(win, ext.win.positions[id])
+  end
 end
 
 -- cycle application windows
 -- originally stolen: https://github.com/nifoc/dotfiles/blob/master/hydra/cycle.lua
 function ext.win.cycle(win)
-	local windows = win:application():allwindows()
-	windows = fnutils.filter(windows, function(win) return win:isstandard() end)
+  local windows = win:application():allwindows()
+  windows = fnutils.filter(windows, function(win) return win:isstandard() end)
 
-	if #windows >= 2 then
-		table.sort(windows, function(a, b) return a:id() < b:id() end)
-		local activewindowindex = fnutils.indexof(windows, win)
+  if #windows >= 2 then
+    table.sort(windows, function(a, b) return a:id() < b:id() end)
+    local activewindowindex = fnutils.indexof(windows, win)
 
-		if activewindowindex then
-			activewindowindex = activewindowindex + 1
-			if activewindowindex > #windows then activewindowindex = 1 end
+    if activewindowindex then
+      activewindowindex = activewindowindex + 1
+      if activewindowindex > #windows then activewindowindex = 1 end
 
-			windows[activewindowindex]:focus()
-		end
-	end
+      windows[activewindowindex]:focus()
+    end
+  end
 end
 
 -- smart browser launch or focus
 ext.app.browser = function()
-	local browsers = { "Safari", "Google Chrome" }
-	local runningapps = application.runningapplications()
+  local browsers = { "Safari", "Google Chrome" }
+  local runningapps = application.runningapplications()
 
-	local runningbrowsers = fnutils.map(browsers, function(browser)
-		return fnutils.find(runningapps, function(app)
-			return app:title() == browser
-		end)
-	end)
+  local runningbrowsers = fnutils.map(browsers, function(browser)
+    return fnutils.find(runningapps, function(app)
+      return app:title() == browser
+    end)
+  end)
 
-	if #runningbrowsers > 0 then
-		runningbrowsers[1]:activate()
-	else
-		application.launchorfocus(browsers[1])
-	end
-end
-
--- exec command
-ext.utils.exec = function(command)
-	local handle = io.popen(os.getenv("SHELL") .. " -l -i -c \"" .. command .. "\"", "r")
-	local output = handle:read("*all")
-	handle:close()
-
-	return output
-end
-
--- toggle bluetooth
-ext.utils.togglebluetooth = function()
-	local status = string.len(ext.utils.exec("blueutil | grep 'Power: 1'")) > 0
-	local command = "blueutil power " .. (status and "0" or "1")
-
-	notification.show("Bluetooth", "Turned " .. (status and "off" or "on"))
-	ext.utils.exec(command)
-end
-
--- toggle wifi
-ext.utils.togglewifi = function()
-	local status = string.len(ext.utils.exec("networksetup -getairportpower en1 | grep On")) > 0
-	local command = "networksetup -setairportpower en1 " .. (status and "off" or "on")
-
-	notification.show("Network", "Turned " .. (status and "off" or "on"))
-	ext.utils.exec(command)
+  if #runningbrowsers > 0 then
+    runningbrowsers[1]:activate()
+  else
+    application.launchorfocus(browsers[1])
+  end
 end
 
 -- apply function to a window with optional params, saving it's position for restore
 function dowin(fn, param)
-	local win = window.focusedwindow()
+  local win = window.focusedwindow()
 
-	if win and not win:isfullscreen() then
-		ext.win.pos(win, "save")
-		fn(win, param)
-	end
+  if win and not win:isfullscreen() then
+    ext.win.pos(win, "save")
+    fn(win, param)
+  end
 end
 
 -- for simple hotkey binding
 function bindwin(fn, param)
-	return function()
-		dowin(fn, param)
-	end
+  return function()
+    dowin(fn, param)
+  end
 end
 
 -- apply function to a window with a timer
 function timewin(fn, param)
-	return timer.new(0.05, function()
-		dowin(fn, param)
-	end)
+  return timer.new(0.05, function()
+    dowin(fn, param)
+  end)
 end
 
 -- keyboard modifier for bindings
 local mod1 = { "cmd", "ctrl" }
 local mod2 = { "cmd", "ctrl", "alt" }
 local mod3 = { "cmd", "alt", "shift" }
+local mod4 = { "cmd", "alt" }
 
 -- basic bindings
 hotkey.bind(mod1, "c",   bindwin(ext.win.center))
@@ -374,45 +391,43 @@ hotkey.bind(mod1, "z",   bindwin(ext.win.full))
 hotkey.bind(mod1, "s",   bindwin(ext.win.pos, "update"))
 hotkey.bind(mod1, "r",   bindwin(ext.win.pos, "load"))
 hotkey.bind(mod1, "w",   bindwin(ext.win.cycle))
-hotkey.bind(mod1, "tab", bindwin(ext.win.throw))
+
+hotkey.bind(mod1, "tab", bindwin(ext.win.throw, "next"))
+hotkey.bind(mod4, "tab", bindwin(ext.win.throw, "prev"))
 
 -- push to edges and nudge
 fnutils.each({ "up", "down", "left", "right" }, function(direction)
-	local nudge = timewin(ext.win.nudge, direction)
+  local nudge = timewin(ext.win.nudge, direction)
 
-	hotkey.bind(mod1, direction, bindwin(ext.win.pushandnudge, direction))
-	hotkey.bind(mod2, direction, function() nudge:start() end, function() nudge:stop() end)
-	hotkey.bind(mod3, direction, bindwin(ext.win.send, direction))
+  hotkey.bind(mod1, direction, bindwin(ext.win.pushandnudge, direction))
+  hotkey.bind(mod2, direction, function() nudge:start() end, function() nudge:stop() end)
+  hotkey.bind(mod3, direction, bindwin(ext.win.send, direction))
 end)
 
 -- set window sizes
 fnutils.each({
-	{ key = "1", w = 1400, h = 940 },
-	{ key = "2", w = 980,  h = 920 },
-	{ key = "3", w = 800,  h = 880 },
-	{ key = "4", w = 800,  h = 740 },
-	{ key = "5", w = 700,  h = 740 },
-	{ key = "6", w = 850,  h = 620 },
-	{ key = "7", w = 770,  h = 470 }
+  { key = "1", w = 1400, h = 940 },
+  { key = "2", w = 980,  h = 920 },
+  { key = "3", w = 800,  h = 880 },
+  { key = "4", w = 800,  h = 740 },
+  { key = "5", w = 700,  h = 740 },
+  { key = "6", w = 850,  h = 620 },
+  { key = "7", w = 770,  h = 470 }
 }, function(object)
-	hotkey.bind(mod1, object.key, bindwin(ext.win.size, { w = object.w, h = object.h }))
+  hotkey.bind(mod1, object.key, bindwin(ext.win.size, { w = object.w, h = object.h }))
 end)
 
 -- launch and focus applications
 fnutils.each({
-	{ key = "c", app = "Calendar" },
-	{ key = "f", app = "Finder" },
-	{ key = "n", app = "Notational Velocity" },
-	{ key = "p", app = "TaskPaper" },
-	{ key = "r", app = "Reminders" },
-	{ key = "t", app = "Terminal" }
+  { key = "c", app = "Calendar" },
+  { key = "f", app = "Finder" },
+  { key = "n", app = "Notational Velocity" },
+  { key = "p", app = "TaskPaper" },
+  { key = "r", app = "Reminders" },
+  { key = "t", app = "Terminal" }
 }, function(object)
-	hotkey.bind(mod2, object.key, function() application.launchorfocus(object.app) end)
+  hotkey.bind(mod2, object.key, function() application.launchorfocus(object.app) end)
 end)
 
 -- launch or focus browser in a smart way
 hotkey.bind(mod2, "b", function() ext.app.browser() end)
-
--- toggle bluetooth and wifi
-hotkey.bind(mod3, "b", function() ext.utils.togglebluetooth() end)
-hotkey.bind(mod3, "w", function() ext.utils.togglewifi() end)
